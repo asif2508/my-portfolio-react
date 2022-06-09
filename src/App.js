63d41f1c53693c3d7248cbf4a93ae3fa253b12da
asif2508 <47877578+asif2508@users.noreply.github.com>
2022-06-09 21:23:27 +0600

    header creation done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
+    <div className="App md:px-12 lg: px-12">
+      <Home></Home>
     </div>
   );
 }
