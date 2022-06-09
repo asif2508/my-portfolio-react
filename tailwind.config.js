d878c58d0949f9921f82dffe1971567f13ae8156
asif2508 <47877578+asif2508@users.noreply.github.com>
2022-06-09 20:52:18 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#370875",
+
+          "secondary": "#3fdd39",
+
+          "accent": "#0c4366",
+
+          "neutral": "#342135",
+
+          "base-100": "#242343",
+
+          "info": "#64C5DD",
+
+          "success": "#7AE6DD",
+
+          "warning": "#F6D46F",
+
+          "error": "#EE3F68",
+        },
+      },
+    ],
+  },
+  plugins: [],
+}
